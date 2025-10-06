23a65ab89e5b9499ce86aafdd187b1c25c1b8cd6
Baidar Bakhat <baidarbutt533@gmail.com>
2025-10-06 23:37:00 +0500

    hiii

--- lib/statfull_widget.dart
+++ lib/statfull_widget.dart
@@ -3,15 +3,34 @@ import 'package:flutter/material.dart';
 class StatfullWidget extends StatefulWidget {
   StatfullWidget({super.key});
 
-  final int x = 10;
-
   @override
   State<StatfullWidget> createState() => _StatfullWidgetState();
 }
 
 class _StatfullWidgetState extends State<StatfullWidget> {
+  int x = 10;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text(widget.x.toString())]));
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(child: Text(x.toString())),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  x++;
+                  print(x);
+                });
+              },
+              child: Text("Click"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
